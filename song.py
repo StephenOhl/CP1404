ed9ef22b6c77017d9f069b53978dd3e5f54a60bf
jc468673 <stephen.ohl@mt.jcu.edu.au>
2017-10-10 11:43:15 +1000

    Changed the song init method to cater for the is_required attribute being passed in as either a 'y', an 'n' or a boolean also made changes to the app.kv file

--- song.py
+++ song.py
@@ -2,11 +2,16 @@
 
 
 class Song:
-    def __init__(self, title='', artist='', year=0, is_required='n'):
+    def __init__(self, title='', artist='', year=0, is_required='y'):
         self.artist = artist
         self.title = title
         self.year = int(year)
-        self.is_required = is_required
+        if is_required == 'y' or is_required == 'Y':
+            self.is_required = True
+        elif is_required == 'n' or is_required == 'N':
+            self.is_required = False
+        else:
+            self.is_required = is_required
 
     def __str__(self):
         return "{} by {} ({:4}) {}".format(self.artist, self.title, self.year, self.is_required)
